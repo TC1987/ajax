62df4a813363e3b01387a288892c79bec423b0fb
StockDC2 <StockDC2@gmail.com>
2016-12-13 16:06:49 -0800

    add editing and deleting functionality

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     app = express(),
     path = require('path'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    _ = require('./views/static/underscore.js');
 
 console.log('dirname: ' + __dirname);
 
@@ -10,14 +11,19 @@ app.use(express.static(path.join(__dirname, 'views')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
-}))
+}));
+
+// ONLY FOR TESTING PURPOSES. DON'T JUDGE ME.
+var _id = 3;
 
 var orders = [
     {
+        id: 1,
         name: 'Steve',
         drink: 'Water'
     },
     {
+        id: 2,
         name: 'Eric',
         drink: 'Coke'
     }
@@ -25,21 +31,20 @@ var orders = [
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
-})
+});
 
 app.get('/orders', function(req, res) {
     res.json(orders);
-})
+});
 
 app.post('/orders', function(req, res) {
-    // console.log('ARG');
     console.log('In /orders path.');
+    req.body.id = _id++;
     orders.push(req.body);
     res.json(req.body);
-})
+});
 
-// WHY DID I WRITE THESE. Should have known that you don't need to specify routes for static files. Just use express.static and don't forget
-// that when using path.join, the 2nd argument is the folder, NOT THE PATH TO THE FOLDER.
+// WHY DID I WRITE THESE. Should have known that the 2nd argument in express.static(path.join()) is the folder, NOT THE PATH TO THE FOLDER. SO MUCH STRESS!
 // app.get('/static/index.js', function(req, res) {
 //     res.sendFile(path.join(__dirname, '/views/index.js'));
 // })
@@ -48,6 +53,26 @@ app.post('/orders', function(req, res) {
 //     res.sendFile(path.join(__dirname, '/node_modules/mustache/mustache.js'));
 // })
 
+app.delete('/orders/:id', function(req, res) {
+    // req.params.id is a string, not a number.
+    orders = _.reject(orders, function(order) {
+        return order.id === parseInt(req.params.id);
+    });
+
+    res.json(orders);
+});
+
+app.put('/orders/:id', function(req, res) {
+    if (req.body) {
+        var id = req.params.id;
+        req.body.id = id;
+        orders[id - 1] = req.body;
+        res.json(req.body);
+    } else {
+        res.json('Failure');
+    }
+})
+
 app.listen(8000, function() {
     console.log('Listening on Port 8000');
-})
+});
--- views/static/index.js
+++ views/static/index.js
@@ -1,19 +1,29 @@
+// .on() is OP!! You're able to delegate events to a parent object that listens for an event on behalf of its children. When an event occurs,
+// you can check if the event is a result of something happening (e.g click) to a specific child (which is the 2nd argument in the .on method).
+// $('PARENT').on('EVENT', 'CHILD', function() {
+
+// })
+
+// Traversing the DOM is expensive so get node and store it in a variable for quick/easy reference.
 var $orders = $('#orders'),
     $name = $('#name'),
     $drink = $('#drink');
 
-var orderTemplate = '<li>Name: {{ name }}</li><li>Drink: {{ drink }}</li>'
+// Get everything within the element that has the id '#order-template'.
+var orderTemplate = $('#order-template').html();
 
+// Add order to homepage. Notice that nothing is being returned because we're just appending.
 function addOrder(order) {
-    $orders.append(mustache.render(orderTemplate, order));
+    $orders.append(Mustache.render(orderTemplate, order));
 }
 
+// AJAX used to get all orders.
 $.ajax({
     type: 'GET',
     url: '/orders',
     success: function(orders) {
         orders.forEach(function(order) {
-            $orders.append('<li>Name: ' + order.name + '</li>' + '<li>Drink: ' + order.drink + '</li>');
+            addOrder(order);
         });
     },
     error: function() {
@@ -40,11 +50,62 @@ $('#add-order').on('click', function() {
         },
         success: function(data) {
             console.log(data);
-            $orders.append('<li>Name: ' + order.name + '</li>' + '<li>Drink: ' + order.drink + '</li>');
+            addOrder(data);
         },
         error: function() {
             console.log('Error saving order.');
         }
-    })
+    });
+});
 
+// Delegating click events to the parent element that has the id '#orders'. Every time event occurs, function checks to see
+// if event is propagating/bubbling from the element with class '.remove'.
+$('#orders').on('click', '.remove', function() {
+    var self = $(this).closest('li'),
+        $id = self.attr('data-id');
+
+    $.ajax({
+        type: 'DELETE',
+        url: '/orders/' + $id,
+        success: function(orders) {
+            self.fadeOut(300, function() {
+                self.remove();
+            });
+        }
+    });
+});
+
+$('#orders').on('click', '.editOrder, .cancelEdit, .saveEdit', function(event) {
+    var $li = $(this).closest('li');
+
+    $li.find('.noEdit').toggle();
+    $li.find('.onEdit').toggle();
+
+    if (event.target.innerHTML === 'Edit') {
+        $li.find('.name').val( $li.find('#name').html() );
+        $li.find('.drink').val( $li.find('#drink').html() );
+    }
+})
+
+$('#orders').on('click', '.saveEdit', function() {
+    // Better to not add ID property to editedOrder. App would be vulnerable to malicious edits.
+    var self = $(this).closest('li'),
+        editedOrder = {
+            name: self.find('.name').val(),
+            drink: self.find('.drink').val()
+        };
+
+    $.ajax({
+        type: 'PUT',
+        url: '/orders/' + self.attr('data-id'),
+        data: editedOrder,
+        success: function(data) {
+            // Option #1: Rerender everything.
+            // Option #2: Target specific order and update values.
+            // Option #3: Delete the li and append a new one. Would put it at the bottom of the list though.
+            var $li = self.parent().find('[data-id="' + data.id + '"]');
+            $li.find('#name').html(data.name);
+            $li.find('#drink').html(data.drink);
+        }
+    })
 })
